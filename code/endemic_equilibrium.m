e5e4580065503a1468c308d425e0bdf88e9f4ecd
CarmenLegarre <carmenlegarreta@hotmail.com>
2020-10-13 16:49:53 +0200

    I implement the system numerically, but the result of S is negative (it is impossible). I have to review, 1- if the system is well build 2- if the in process of transforming the non linear system into linear system there has been any failure 3- if it well implemented in matlab

--- code/endemic_equilibrium.m
+++ code/endemic_equilibrium.m
@@ -7,10 +7,44 @@
 % simulation random values will be asigned to these variables.
 
 %% Constants definition
-la = rand(); rho = rand(); be = rand(); bs = rand(); ba = rand();
-p2 = rand();  p1 = rand(); mu = rand(); sig = rand(); gamma = rand();
+la = 0.5; rho = rand(); be = rand(); bs = rand(); ba = rand();
+p2 = rand();  p1 = rand(); mu = rand(); sigma = rand(); gamma = rand();
 alpha = rand(); 
 
 %% Manual solution
 
+
 %% Numerical solution
+system_a = zeros (5, 5);
+
+% first array
+system_a(1, 3) = (mu + sigma + gamma*p2)*(1-p1);
+system_a(1, 2) = -(sigma + mu)*p1;
+
+% second array 
+system_a(2, 1) = -mu;
+system_a(2, 2) = -(sigma + mu);
+system_a(2, 3) = -(mu + sigma + gamma*p2);
+system_a(2, 5) =  rho;
+
+% third array
+system_a(3, 2) = sigma;
+system_a(3, 3) = sigma*(1 - p2);
+system_a(3, 4) = -(mu + alpha*(1 - p2) + gamma);
+
+% fourth array
+system_a(4, 3) = (sigma + gamma)*p2;
+system_a(4, 4) = gamma;
+system_a(4, 5) = -(mu + rho);
+
+% fifth array
+system_a(5, 2) = 1;
+
+% system
+system_b = [0, -la, 0, 0, 0.5]';
+
+% solution
+solution = linsolve(system_a, system_b);
+
+
+
