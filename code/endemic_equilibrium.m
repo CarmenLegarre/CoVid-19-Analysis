31671d2fd22e240ca5aaa9f507c444e9bdf8b41d
CarmenLegarre <carmenlegarreta@hotmail.com>
2020-10-13 15:17:21 +0200

    Define which is the goal of the script and variables definition.

---
+++ code/endemic_equilibrium.m
@@ -0,0 +1,16 @@
+% Date: 13/10/2020
+% Author: Carmen Legarreta
+% 
+% Create an script to prove that the obtained results from the system in
+% the endemic equilibium case are the same as the obtained manually. The
+% system equations depende of variables which values are unknown, for the
+% simulation random values will be asigned to these variables.
+
+%% Constants definition
+la = rand(); rho = rand(); be = rand(); bs = rand(); ba = rand();
+p2 = rand();  p1 = rand(); mu = rand(); sig = rand(); gamma = rand();
+alpha = rand(); 
+
+%% Manual solution
+
+%% Numerical solution
