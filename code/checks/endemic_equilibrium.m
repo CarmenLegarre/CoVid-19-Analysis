e63864a80bbf7d78e7280c6fb05a0f0827b22e84
CarmenLegarre <carmenlegarreta@hotmail.com>
2020-11-26 12:08:55 +0100

    The code has been changed to match with the updated model. I checked the results, and it seem that the code is valid.

--- code/checks/endemic_equilibrium.m
+++ code/checks/endemic_equilibrium.m
@@ -8,48 +8,50 @@
 
 %% Constants definition
 la = 1000; rho = rand(); be = rand(); bs = rand(); ba = rand();
-p2 = rand();  p1 = rand(); mu = rand(); sigma = rand(); gamma = rand();
-alpha = rand(); 
+p2 = rand();  p1 = rand(); p3 = rand();  mu = rand(); sigma = rand(); 
+gamma = rand(); alpha = rand(); delta = rand();
 
 %% Manual solution
 e = 100;
 
-s = 1/mu * (rho*e/(mu + rho)*((gamma/(mu+alpha*(1-p2)+gamma)* ...
-    (1+(sigma+mu)*(1-p2)*p1/(mu+sigma+gamma*p2)/(1-p1))*sigma) + ... 
-    (sigma + mu)*(sigma + gamma)*p1*p2/(mu+sigma+gamma*p2)/(1-p1)) - ...
-    (sigma + mu)*e/(1-p1)  + la);
+s = 1.0 / mu * (rho* (gamma - p3 * delta) / (rho + mu) / ...
+    (mu + (1 - p2) * alpha + gamma - p3*delta) * ((sigma + p1 * p3 * ...
+    delta * (sigma + mu) / (1 - p1) / (mu + (1 - p2) * alpha + p3 * ...
+    delta)) + p1 * (sigma + mu) * delta * (1 - p3) / (gamma - p3 * delta))- ...
+    (sigma + mu)/(1 - p1)) * e + la/mu ;
 
-q = (sigma + mu)*p1*e/(sigma + mu + gamma*p2)/(1-p1);
+q = (sigma + mu)*p1*e/(mu + (1-p2) * alpha + p3 * delta)/(1-p1);
 
-i = (1 + (sigma + mu)*(1 - p2)*p1/(sigma + mu + gamma*p2)/(1 - p1))* ...
-    e*sigma/(mu + alpha*(1 - p2) + gamma); 
+i = (sigma + p1 * p3 * delta * (sigma + mu) / (1 - p1) / ...
+    (mu + (1 - p2) * alpha + p3 *delta)) * e / (mu + (1 - p2) * alpha + ...
+    gamma - p3 * delta); 
 
-r = e/(mu + rho)*((gamma/(mu+alpha*(1-p2)+gamma)*(1+(sigma+mu)*(1-p2)* ...
-    p1/(mu+sigma+gamma*p2)/(1-p1))*sigma) + (sigma + mu)* ...
-    (sigma + gamma)*p1*p2/(mu+sigma+gamma*p2)/(1-p1));
+r = ((gamma - p3 * delta)*(sigma + p1 * p3 * delta * (sigma + mu) / (1 - p1) / ...
+    (mu + (1 - p2) * alpha + p3 *delta)) + p1 * (sigma + mu) * delta * ...
+    (1 - p3)) * e / (rho + mu) / (mu + (1 - p2) * alpha + gamma - p3 * delta);
 
 
 %% Numerical solution
 system_a = zeros (5, 5);
 
-% first array
-system_a(1, 3) = (mu + sigma + gamma*p2)*(1-p1);
+% first array, done
+system_a(1, 3) = (mu + (1 - p2) * alpha + p3 * delta)*(1-p1);
 system_a(1, 2) = -(sigma + mu)*p1;
 
-% second array 
+% second array, done
 system_a(2, 1) = -mu;
 system_a(2, 2) = -(sigma + mu);
-system_a(2, 3) = -(mu + sigma + gamma*p2);
+system_a(2, 3) = -(mu + (1 - p2) * alpha + p3 * delta);
 system_a(2, 5) =  rho;
 
-% third array
+% third array, done
 system_a(3, 2) = sigma;
-system_a(3, 3) = sigma*(1 - p2);
-system_a(3, 4) = -(mu + alpha*(1 - p2) + gamma);
+system_a(3, 3) = delta * p3;
+system_a(3, 4) = -(mu + alpha*(1 - p2) + gamma - p3 * delta);
 
-% fourth array
-system_a(4, 3) = (sigma + gamma)*p2;
-system_a(4, 4) = gamma;
+% fourth array, done
+system_a(4, 3) = delta * (1 - p3);
+system_a(4, 4) = (gamma - p3 * delta);
 system_a(4, 5) = -(mu + rho);
 
 % fifth array
