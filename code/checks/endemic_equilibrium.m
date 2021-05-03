3f23bd7cc27278c32f6e3f9599c340a918440e42
CarmenLegarre <carmenlegarreta@hotmail.com>
2021-05-03 15:20:39 +0200

    prueba

--- code/checks/endemic_equilibrium.m
+++ code/checks/endemic_equilibrium.m
@@ -7,58 +7,58 @@
 % simulation random values will be asigned to these variables.
 
 %% Constants definition
-la = 1000; rho = rand(); be = rand(); bs = rand(); ba = rand();
+La = 1000; rho = rand(); be = rand(); bs = rand(); ba = rand();
 p2 = rand();  p1 = rand(); p3 = rand();  mu = rand(); sigma = rand(); 
-gamma = rand(); alpha = rand(); delta = rand();
+gamma = rand(); alpha = rand(); delta = rand(); la = rand();
 
 %% Manual solution
+b1 = (la*(1-p2) + mu + gamma);
+b2 = (la*(1-p2)/b1 + (sigma + mu) * (1-p2) * p1 * la * bs)/(b1 * be + ...
+    (ba * p2 + (bs * (1-p2) * (1 - p1 * la))) * sigma) / (mu + delta + alpha);
+
 e = 100;
 
-s = 1.0 / mu * (rho* (gamma - p3 * delta) / (rho + mu) / ...
-    (mu + (1 - p2) * alpha + gamma - p3*delta) * ((sigma + p1 * p3 * ...
-    delta * (sigma + mu) / (1 - p1) / (mu + (1 - p2) * alpha + p3 * ...
-    delta)) + p1 * (sigma + mu) * delta * (1 - p3) / (gamma - p3 * delta))- ...
-    (sigma + mu)/(1 - p1)) * e + la/mu ;
+s = La/mu + sigma * (b2 * ((rho * delta)/(rho + mu) - mu - alpha ...
+    - delta) + ((rho * gamma)/(rho + mu) + la * (1 - p2))/b1 - ...
+    (sigma + mu)/sigma) * e / mu ;
 
-q = (sigma + mu)*p1*e/(mu + (1-p2) * alpha + p3 * delta)/(1-p1);
+q = sigma * b2 * e;
 
-i = (sigma + p1 * p3 * delta * (sigma + mu) / (1 - p1) / ...
-    (mu + (1 - p2) * alpha + p3 *delta)) * e / (mu + (1 - p2) * alpha + ...
-    gamma - p3 * delta); 
+i = sigma * e / b1; 
 
-r = ((gamma - p3 * delta)*(sigma + p1 * p3 * delta * (sigma + mu) / (1 - p1) / ...
-    (mu + (1 - p2) * alpha + p3 *delta)) + p1 * (sigma + mu) * delta * ...
-    (1 - p3)) * e / (rho + mu) / (mu + (1 - p2) * alpha + gamma - p3 * delta);
+r = sigma * (gamma / b1 + delta * b2) * e/ (rho + mu);
 
 
 %% Numerical solution
+% x = S E Q I R
+
 system_a = zeros (5, 5);
 
 % first array
-system_a(1, 3) = (mu + (1 - p2) * alpha + p3 * delta)*(1-p1);
-system_a(1, 2) = -(sigma + mu)*p1;
+system_a(1, 2) = -sigma * b1 * b2 * (mu + alpha + delta);
+system_a(1, 3) =  b1 * (mu + alpha + delta);
 
 % second array
 system_a(2, 1) = -mu;
 system_a(2, 2) = -(sigma + mu);
-system_a(2, 3) = -(mu + (1 - p2) * alpha + p3 * delta);
+system_a(2, 3) = -(mu + alpha + delta);
+system_a(2, 4) = la * (1 - p2);
 system_a(2, 5) =  rho;
 
 % third array
 system_a(3, 2) = sigma;
-system_a(3, 3) = delta * p3;
-system_a(3, 4) = -(mu + alpha*(1 - p2) + gamma - p3 * delta);
+system_a(3, 4) = -(la * (1 - p2) + mu + gamma);
 
 % fourth array
-system_a(4, 3) = delta * (1 - p3);
-system_a(4, 4) = (gamma - p3 * delta);
+system_a(4, 3) = delta;
+system_a(4, 4) = gamma;
 system_a(4, 5) = -(mu + rho);
 
 % fifth array
 system_a(5, 2) = 1;
 
 % system
-system_b = [0, -la, 0, 0, e]';
+system_b = [0, -La, 0, 0, e]';
 
 % solution
 solution = linsolve(system_a, system_b);
